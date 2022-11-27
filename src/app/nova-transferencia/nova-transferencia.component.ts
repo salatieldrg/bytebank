5f74a1798c8cd55dd3cb2bcd4ef2c9e3615f0d0c
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-27 12:06:58 -0300

    Transferindo dados entre componentes e formatando campos

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,5 +6,21 @@ import { Component } from "@angular/core";
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent{
+
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor : number;
+    destino : number;
+
+    transferir(){
+      const valorEmitir = { valor: this.valor, destino: this.destino};
+      this.aoTransferir.emit(valorEmitir);
+      this.limpar();
+    }
+
+    limpar(){
+      this.valor = 0;
+      this.destino = 0;
+    }
 
 }
