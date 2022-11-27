53ba44116b9e1810853cbdc20780c7ecce55393b
Salatiel dos Reis <salatieldrg@gmail.com>
2022-11-27 17:34:35 -0300

    Listando dados de uma API

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,5 +1,6 @@
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
 
 @Component({
   selector: 'app-extrato',
@@ -13,7 +14,11 @@ export class ExtratoComponent implements OnInit {
   constructor(private service: TransferenciaService) { }
 
   ngOnInit(): void {
-    this.transferencias = this.service.transferencias;
+    this.service.todas().subscribe((transferencias: Transferencia[]) =>{
+      console.table(transferencias);
+      this.transferencias = transferencias;
+    });
+
   }
 
 
--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Transferencia } from '../models/transferencia.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +9,18 @@ import { Injectable } from '@angular/core';
 export class TransferenciaService {
 
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-constructor() {
+constructor(private httpclient: HttpClient) {
   this.listaTransferencia = [];
  }
 
  get transferencias (){
   return this.listaTransferencia;
+ }
+
+ todas(): Observable<Transferencia[]>{
+  return this.httpclient.get<Transferencia[]>(this.url);
  }
 
  adicionar(transferencia: any){
